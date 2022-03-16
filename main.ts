99b55df23a6354c7b03404e66f675bce0a85bd7d
samuelmardare <101750448+samuelmardare@users.noreply.github.com>
2022-03-16 21:20:43 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,24 @@
+let velocidad = 50
+basic.forever(function () {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
+    } else {
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, velocidad)
+            if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, velocidad)
+            }
+        } else {
+            if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, velocidad)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+                if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+                    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, velocidad)
+                    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+                }
+            }
+        }
+    }
+})
